a67fe3beb8004b8e35e172233d275debe922d243
Hiago de Oliveira Mendes <99771272+hiag0liveira@users.noreply.github.com>
2023-02-12 14:26:24 -0300

    Delete upcar directory

--- upcar/src/main/java/hiag0liveira/github/io/upcar/domain/repositorio/ItemsPedido.java
+++
@@ -1,8 +0,0 @@
-package hiag0liveira.github.io.upcar.domain.repositorio;
-
-
-import hiag0liveira.github.io.upcar.domain.entity.ItemPedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemsPedido extends JpaRepository<ItemPedido, Integer> {
-}
