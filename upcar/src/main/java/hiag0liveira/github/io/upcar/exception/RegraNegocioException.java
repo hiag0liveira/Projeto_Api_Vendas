9a554c411c6e505b43ee3507ee7ce881bc564479
Hiago de Oliveira Mendes <99771272+hiag0liveira@users.noreply.github.com>
2023-03-08 20:44:46 -0300

    Delete upcar directory

--- upcar/src/main/java/hiag0liveira/github/io/upcar/exception/RegraNegocioException.java
+++
@@ -1,8 +0,0 @@
-package hiag0liveira.github.io.upcar.exception;
-
-public class RegraNegocioException extends RuntimeException {
-
-    public RegraNegocioException(String message) {
-        super(message);
-    }
-}
