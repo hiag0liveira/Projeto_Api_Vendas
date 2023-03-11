642e66e522c0d2e4c63cfe7f2cea696c7a55598c
Hiago de Oliveira Mendes <99771272+hiag0liveira@users.noreply.github.com>
2023-03-11 12:25:37 -0300

    Delete upcar directory

--- upcar/src/main/java/hiag0liveira/github/io/upcar/domain/repositorio/Produtos.java
+++
@@ -1,7 +0,0 @@
-package hiag0liveira.github.io.upcar.domain.repositorio;
-
-import hiag0liveira.github.io.upcar.domain.entity.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Produtos  extends JpaRepository<Produto,Integer> {
-}
